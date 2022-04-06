8c8587703dca2b52539e6f6ec45f5277727ec5ca
zpayne1776 <zacharydpayne1@gmail.com>
2022-04-05 22:23:18 -0600

    Created ImportActivity and activity_import.xml. Still need to flesh out

---
+++ app/src/main/java/android/Contacts3/ImportActivity.java
@@ -0,0 +1,14 @@
+package android.Contacts3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import);
+    }
+}
